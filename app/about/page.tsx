30918f07e5089d59a186a6c69b0c6cdb97602be1
emanuelefavero <emafaux89@gmail.com>
2023-05-08 12:40:21 +0200

    add fetch from api

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
-export default function AboutPage() {
-  return <div>AboutPage</div>
+type User = {
+  username: string
+}
+
+async function getUser() {
+  const res = await fetch('http://localhost:3000/api/user')
+  return res.json()
+}
+
+export default async function AboutPage() {
+  const user: User = await getUser()
+  return <div>AboutPage, {user.username}</div>
 }
