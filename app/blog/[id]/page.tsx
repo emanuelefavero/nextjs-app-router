be4492fe67c3ec228ecd4ab3b549213ab99be910
emanuelefavero <emafaux89@gmail.com>
2024-03-13 17:00:43 +0100

    add jsonplaceholder api in blog

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,6 +4,24 @@ type Props = {
   }
 }
 
-export default function page({ params }: Props) {
-  return <h1>Blog {params.id}</h1>
+type Post = {
+  title: string
+  body: string
+}
+
+async function getPost(id: string) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  return res.json()
+}
+
+export default async function page({ params }: Props) {
+  const post: Post = await getPost(params.id)
+
+  return (
+    <>
+      <h1>Blog {params.id}</h1>
+      <h3>{post.title}</h3>
+      <p>{post.body}</p>
+    </>
+  )
 }
