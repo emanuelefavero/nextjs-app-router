e3653ed39f3a00c88cfbc90e92afbb69fe6839e9
emanuelefavero <emafaux89@gmail.com>
2024-03-15 15:41:14 +0100

    add redirect from config file

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { logUsername } from '@/app/actions'
 
 // TIP: Check logUsername function in app/actions.tsx to see how to use the redirect function
@@ -5,6 +6,9 @@ import { logUsername } from '@/app/actions'
 export default function Page() {
   return (
     <>
+      <h1>Redirect</h1>
+
+      <h2 className='mt-10'>Redirect after server action</h2>
       <p>
         The user will be redirected home after submitting the form. Check the
         server console to see the name logged
@@ -13,6 +17,13 @@ export default function Page() {
         <input type='text' name='username' placeholder='Username' />
         <button type='submit'>Log Username</button>
       </form>
+
+      <h2 className='mt-10'>Redirect from config file</h2>
+      <p>
+        The user will be redirected home. The redirect path is being configured
+        in the next.config.js file
+      </p>
+      <Link href='/redirect-from-config-file'>Redirect from config file</Link>
     </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/redirect-from-config-file',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
