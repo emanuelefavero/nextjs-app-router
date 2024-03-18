a30a699ce09a69fd308c6a7e619aef75156d2179
emanuelefavero <emafaux89@gmail.com>
2024-03-18 20:17:41 +0100

    add - log all data in server actions

--- app/server-actions/page.tsx
+++ app/server-actions/page.tsx
@@ -4,7 +4,12 @@ export default function Page() {
   async function createUser(formData: FormData) {
     'use server'
 
+    // Log the user name to the server console
     console.log(formData.get('name'))
+
+    // Log all form data to the server console
+    const rawFormData = Object.fromEntries(formData)
+    console.log(rawFormData)
 
     // HERE: Do something with the form data in the server
 
@@ -29,3 +34,7 @@ export default function Page() {
     </div>
   )
 }
+
+// TIP: You can pass additional data to a server action with the bind method
+// @see https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations
+// TIP: To use server actions in Client Components, create a separate actions.ts file with the 'use server' at the top and import it in the client component
