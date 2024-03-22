a8e8127625806f724fe0bc1b3040d82e1f026666
emanuelefavero <emafaux89@gmail.com>
2024-03-22 19:12:05 +0100

    add tips to images

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -15,3 +15,12 @@ export default function page() {
 
 // TIP: Images imported this way (from local server) are optimized by default and don't need the width and height attributes
 // TIP: Use the priority attribute in the image that is the "Largest Contentful Paint Element" (the first image that appears on the screen and the largest and most important one)
+// TIP: You can also put external urls in the src attribute or even the public folder with (e.g. '/night-sky.webp')
+// TIP: To expand the image to its parent element use the fill attribute or objectFit attribute with 'fill', 'contain' or 'cover' and objectPosition to position the image (e.g. 'objectFit: 'cover', objectPosition: 'center'). If those don't work, you can still use the <img> tag instead of the Image component
+// TIP: You could also style the image with:
+/*
+style={{
+  width: '100%',
+  height: 'auto',
+}}
+*/
