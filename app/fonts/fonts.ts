849e0891dcba9324a73f2ed0c2795fe5119436b2
emanuelefavero <emafaux89@gmail.com>
2024-03-24 01:24:06 +0100

    remove unused font weights

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -28,15 +28,15 @@ export const noto_serif = Noto_Serif({ subsets: ['latin'] }) // Serif
 // Fixed Fonts need to specify the weights
 export const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '700', '900'],
+  weight: ['400', '500', '700'],
 })
 export const lato = Lato({
   subsets: ['latin'],
-  weight: ['100', '300', '400', '700', '900'],
+  weight: ['400', '700'],
 })
 export const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '900'],
+  weight: ['400', '500', '600', '700'],
 })
 
 // --------------------------------------------------------
