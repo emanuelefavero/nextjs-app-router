82345c96a7223adaac28daccd3505d81d7f0c994
emanuelefavero <emafaux89@gmail.com>
2024-03-24 19:07:14 +0100

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// * Bundle Analyzer is a plugin for Next.js that helps you manage the size of your JavaScript modules
+// INSTALL: npm i @next/bundle-analyzer
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+// Run the following command to analyze your bundles:
+// ANALYZE=true npm run build
+// The report will create the .next build folder and open three new tabs in your browser, which you can inspect.
+// TIP: Delete the .next folder after you are done analyzing, as it can take up a lot of space
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
@@ -11,4 +22,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
