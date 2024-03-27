22acafec2e5f5fff1f102b3b66a7c876ff7deb81
emanuelefavero <emafaux89@gmail.com>
2024-03-27 06:31:10 +0100

    update url in config/server

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://nextjs-app-router-mr0aw1s1g-emanuelefavero.vercel.app'
+  : 'https://nextjs-app-router-rouge.vercel.app'
 
 // ? EXAMPLE VERCEL URL: https://YOUR-APP-NAME.vercel.app
