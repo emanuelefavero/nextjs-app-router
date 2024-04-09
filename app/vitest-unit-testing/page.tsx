64f67dc5f3eb2f160d42f7c9dacccae603cbefa8
emanuelefavero <emafaux89@gmail.com>
2024-04-09 20:03:09 +0200

    update vitest-unit-testing/page

--- app/vitest-unit-testing/page.tsx
+++ app/vitest-unit-testing/page.tsx
@@ -1,14 +1,26 @@
+import Link from 'next/link'
+
 export default function Page() {
+  const pStyle = 'mb-2'
+
   return (
     <>
       <h1>Vitest Unit Testing</h1>
 
-      <p>
+      <p className={pStyle}>
         You can use either Vitest or Jest for unit tests in Next.js. Vitest is
         easier to setup and has the same syntax of Jest.
       </p>
 
-      <p>
+      <p className={pStyle}>
+        Check{' '}
+        <Link href='https://nextjs.org/docs/app/building-your-application/testing/vitest'>
+          Next.js Vitest Documentation
+        </Link>{' '}
+        to see how to setup and run Vitest tests in Next.js
+      </p>
+
+      <p className={pStyle}>
         Check{' '}
         <span className='text-amber-500'>
           {'/vitest-unit-testing'}/page.test.tsx
